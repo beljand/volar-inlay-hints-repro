6b4b854e81d1f1ad5bb35876fc300e0a440743ee
David Beljan <david.beljan@cleanshelf.net>
2023-02-22 12:40:08 +0100

    repro

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <test-component size="sm" />
   </div>
 </template>
 
 <script>
+import { defineComponent } from "@vue/runtime-core"
 import HelloWorld from './components/HelloWorld.vue'
+import TestComponent from './components/TestComponent.vue'
 
-export default {
+export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    TestComponent
   }
-}
+})
 </script>
 
 <style>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>test</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "TestComponent",
+	components: {},
+	props: {
+		size: {
+			type: String,
+			default: "md",
+			validator: function (value: string) {
+				return ["md", "sm"].indexOf(value) > -1;
+			},
+		},
+	},
+	data() {
+		return {
+			test: 'test'
+		};
+	},
+});
+</script>
